306165ee0b0f72101d5bfbb4037089efa0e67e00
Marc89 <drakeunch@hotmail.com>
2017-06-22 13:00:30 +0200

    herokufuck18

--- config/routes.rb
+++ config/routes.rb
@@ -24,10 +24,10 @@ Rails.application.routes.draw do
   resources :charges
   resources :select_movies, only: :index
 
-  require 'sidekiq/web'
-  authenticate :user, lambda { |u| u.admin? } do
-    mount Sidekiq::Web => '/sidekiq'
-  end
+  # require 'sidekiq/web'
+  # authenticate :user, lambda { |u| u.admin? } do
+  #   mount Sidekiq::Web => '/sidekiq'
+  # end
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
